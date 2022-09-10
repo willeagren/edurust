b20ccf17aca3196054e940675af3096b37bfdbe7
Eli Vance <wilhelmagren98@gmail.com>
2022-09-10 11:45:26 +0200

    I dont know

--- book/chapter1/target/debug/chapter1.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/bund/Coding/Github/edurust/book/chapter1/target/debug/chapter1: /mnt/c/Users/bund/Coding/Github/edurust/book/chapter1/src/main.rs
--- book/chapter1/target/debug/deps/chapter1-45ae483c9ec6b33b.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/bund/Coding/Github/edurust/book/chapter1/target/debug/deps/chapter1-45ae483c9ec6b33b: src/main.rs
-
-/mnt/c/Users/bund/Coding/Github/edurust/book/chapter1/target/debug/deps/chapter1-45ae483c9ec6b33b.d: src/main.rs
-
-src/main.rs:
