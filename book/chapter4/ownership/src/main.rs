eea7b88acfe5059c148821bfb53a60d3c6f37d15
willeagren <wilhelmagren98@gmail.com>
2022-09-15 15:20:04 +0200

    Ownership example with swap_by_ref/val for pointers.

--- book/chapter4/ownership/src/main.rs
+++ book/chapter4/ownership/src/main.rs
@@ -1,3 +1,25 @@
+
+// Swap the value of two integers by using the reference to the value and
+// dereferencing it with the other value.
+fn swap_by_ref(a: &mut i32, b: &mut i32) {
+    let tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+fn swap_by_val(mut a: i32, mut b: i32) -> (i32, i32) {
+    (a, b) = (b, a);
+    (a, b)
+}
+
+
 fn main() {
     println!("Hello, world!");
+    let mut a: i32 = 10;
+    let mut b: i32 = 8;
+    println!("a = {}, b = {}", a, b);
+    swap_by_ref(&mut a, &mut b);
+    println!("a = {}, b = {}", a, b);
+    (a, b) = swap_by_val(a, b);
+    println!("a = {}, b = {}", a, b);
 }
